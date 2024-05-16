9c32bf967517fa5e8d9684ebbba16e72353d833d
Jhonny Moreira <jhonnymoreira@protonmail.com>
2024-05-16 15:15:56 -0300

    Hashing: Counting Elements

--- src/hashing/counting-elements.ts
+++ src/hashing/counting-elements.ts
@@ -1,1 +1,12 @@
-export function countElements(nums: number[]): number {}
+export function countElements(arr: number[]): number {
+  const uniqueNumbers = new Set(arr);
+
+  let count = 0;
+  for (const num of arr) {
+    if (uniqueNumbers.has(num + 1)) {
+      count++;
+    }
+  }
+
+  return count;
+}
--- tests/hashing/counting-elements.test.ts
+++ tests/hashing/counting-elements.test.ts
@@ -3,14 +3,14 @@ import { countElements } from '@/hashing/counting-elements.js';
 describe('Hashing: Counting Elements', () => {
   test.each([
     {
-      nums: [1, 2, 3],
-      output: 0,
+      arr: [1, 2, 3],
+      output: 2,
     },
     {
-      nums: [1, 1, 3, 3, 5, 5, 7, 7],
+      arr: [1, 1, 3, 3, 5, 5, 7, 7],
       output: 0,
     },
-  ])('countElements($nums) === $output', ({ nums, output }) => {
-    expect(countElements(nums)).toStrictEqual(output);
+  ])('countElements($nums) === $output', ({ arr, output }) => {
+    expect(countElements(arr)).toStrictEqual(output);
   });
 });
